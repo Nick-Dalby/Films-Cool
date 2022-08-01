5dde62b7fbf5784992b4193b2ddb11f6ce72e032
Nick Dalby <nckdlby@gmail.com>
2022-08-01 11:41:08 +0200

    add movie component

---
+++ src/components/Movie/Movie.js
@@ -0,0 +1,21 @@
+import { Container, Cover, Title, Director } from './styles'
+import { Pill } from '../../styles'
+
+const Movie = ({ movie, isLarge, pickMovie }) => {
+  return (
+    <Container $isLarge={isLarge} onClick={() => pickMovie(movie)}>
+      <Cover
+        src={movie.ImagePath}
+        alt={`Image for the movie ${movie.Title}`}
+        $isLarge={isLarge}
+      />
+
+      <figcaption>
+        <Title $isLarge={isLarge}>{movie.Title}</Title>
+        <Director>Director: {movie.Director.Name}</Director>
+      </figcaption>
+    </Container>
+  )
+}
+
+export default Movie
