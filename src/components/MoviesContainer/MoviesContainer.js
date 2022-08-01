f2b3607ab4ee3d40d77f3e4c5bac28f59dda78fb
Nick Dalby <nckdlby@gmail.com>
2022-08-01 11:41:41 +0200

    add movies container view

---
+++ src/components/MoviesContainer/MoviesContainer.js
@@ -0,0 +1,45 @@
+import { useEffect, useState, useRef } from 'react'
+import { useSelector } from 'react-redux'
+import { Container, H2, MovieList } from './styles'
+import { debounce } from 'lodash-es'
+
+import Movie from '../Movie/Movie'
+
+const MoviesContainer = ({ pickMovie, isPanelOpen }) => {
+  const [scroll, setScroll] = useState(0)
+  const prevPanelState = useRef(false)
+
+  const { movies } = useSelector((state) => state.movies)
+
+  useEffect(() => {
+    const handleScroll = debounce(() => {
+      setScroll(window.scrollY)
+    }, 100)
+
+    if (!isPanelOpen) {
+      window.addEventListener('scroll', handleScroll)
+    }
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [isPanelOpen])
+
+  useEffect(() => {
+    if (prevPanelState.current && !isPanelOpen) {
+      window.scroll(0, scroll)
+    }
+    prevPanelState.current = isPanelOpen
+  }, [isPanelOpen, prevPanelState, scroll])
+
+  return (
+    <Container $isPanelOpen={isPanelOpen} $top={scroll}>
+      <MovieList>
+        {movies.map((movie) => (
+          <Movie key={movie._id} movie={movie} pickMovie={pickMovie} />
+        ))}
+      </MovieList>
+    </Container>
+  )
+}
+
+export default MoviesContainer
