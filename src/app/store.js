34439a2b96af91d107f3bbbabc8ef40fdc686425
Nick Dalby <nckdlby@gmail.com>
2022-08-02 15:50:28 +0200

    add favorites reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
 import moviesReducer from '../features/movies/moviesSlice'
+import userDataReducer from '../features/userData/userDataSlice'
+import favoritesReducer from '../features/favorites/favortiesSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     movies: moviesReducer,
+    userData: userDataReducer,
+    favorites: favoritesReducer,
   },
 })
