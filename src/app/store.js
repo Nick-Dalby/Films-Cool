8b60c3298da01e5902764f5ff6a6e2702e2ab8e8
Nick Dalby <nckdlby@gmail.com>
2022-08-02 17:57:50 +0200

    clean up

--- src/app/store.js
+++ src/app/store.js
@@ -2,13 +2,11 @@ import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
 import moviesReducer from '../features/movies/moviesSlice'
 import userDataReducer from '../features/userData/userDataSlice'
-import favoritesReducer from '../features/favorites/favortiesSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     movies: moviesReducer,
     userData: userDataReducer,
-    favorites: favoritesReducer,
   },
 })
